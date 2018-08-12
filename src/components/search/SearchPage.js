2be43239e3a17acdff6657e5ededd993bd477a40
Kristian Epps <kristian@xepps.com>
2018-08-12 23:08:31 +0100

    Search widget (#5)

--- src/components/search/SearchPage.js
+++ src/components/search/SearchPage.js
@@ -1,5 +1,46 @@
-import React from 'react';
+import 'whatwg-fetch';
+import React, { Component } from 'react';
 
-const IndexPage = () => <div>Search</div>;
+import Search from './Search';
+import Results from './Results';
 
-export default IndexPage;
+import './search.css';
+
+class SearchPage extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            searchResults: null
+        };
+
+        this.searchUpdated = this.searchUpdated.bind(this);
+    }
+
+    searchUpdated(value) {
+        if (value.length > 1) {
+            fetch(
+                `https://cors.io/?https://www.rentalcars.com/FTSAutocomplete.do?solrIndex=fts_en&solrRows=6&solrTerm=${value}`
+            )
+                .then(res => res.json())
+                .then(res => res.results)
+                .then(res => this.setState({ searchResults: res.docs, numFound: res.numFound }));
+        } else {
+            this.setState({ searchResults: null });
+        }
+    }
+
+    render() {
+        return (
+            <div className="search-page">
+                <h2 className="search-page__title">Where are you going?</h2>
+                <Search searchUpdated={this.searchUpdated} />
+                {this.state.searchResults ? (
+                    <Results numFound={this.state.numFound} resultsList={this.state.searchResults} />
+                ) : null}
+            </div>
+        );
+    }
+}
+
+export default SearchPage;
---
+++ test/components/search/SearchPage.spec.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { mount } from 'enzyme';
+
+import fetchMock from 'fetch-mock';
+
+import SearchPage from '../../../src/components/search/SearchPage';
+import Search from '../../../src/components/search/Search';
+import Results from '../../../src/components/search/Results';
+
+import requestResponse from '../../__mocks__/man.json';
+
+describe('The search page', () => {
+    let searchPage;
+
+    beforeEach(() => {
+        fetchMock.mock(
+            'begin:https://cors.io/?https://www.rentalcars.com/FTSAutocomplete.do?solrIndex=fts_en&solrRows=6&solrTerm=',
+            {
+                status: 200,
+                body: requestResponse
+            }
+        );
+
+        searchPage = mount(<SearchPage />);
+    });
+
+    afterEach(() => {
+        fetchMock.restore();
+    });
+
+    it('should have a header', () => {
+        expect(searchPage.find('.search-page__title').text()).toEqual('Where are you going?');
+    });
+
+    it('should contain the search box', () => {
+        expect(searchPage.find(Search).exists()).toBeTruthy();
+    });
+
+    it('should call the rentalcars endpoint on change with 2 or more characters and display results', () => {
+        searchPage.find('.search__input').simulate('change', { target: { value: 'ma' } });
+
+        expect(
+            fetchMock.called(
+                'https://cors.io/?https://www.rentalcars.com/FTSAutocomplete.do?solrIndex=fts_en&solrRows=6&solrTerm=ma'
+            )
+        ).toBeTruthy();
+
+        searchPage.update();
+        expect(searchPage.find(Results)).toBeTruthy();
+    });
+
+    it('should not call the rentalcars endpoint with less than 2 characters and not display results', () => {
+        searchPage.find('.search__input').simulate('change', { target: { value: 'm' } });
+
+        expect(
+            fetchMock.called(
+                'https://cors.io/?https://www.rentalcars.com/FTSAutocomplete.do?solrIndex=fts_en&solrRows=6&solrTerm=m'
+            )
+        ).toBeFalsy();
+
+        searchPage.update();
+        expect(searchPage.find(Results).exists()).toBeFalsy();
+    });
+});
